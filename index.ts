4105e8d8bd9219705de309731d9be91c55ef0ad1
Vishal_tripathi <vishal.tripathi@flairlabs.com>
2026-04-01 00:05:53 +0530

    setup mongodb

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
-import express from 'express';
+import express from "express";
+import {connectDB} from "./src/config/db";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/health", (req, res) => {
+  res.send("OK");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(3000, () => {
+    console.log("Server running on port 5000");
+  });
+};
+
+startServer();
